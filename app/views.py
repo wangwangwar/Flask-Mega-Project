09ff61cdd7dead6c97557b65fdf4fac4d44e2607
Wang <wangwangwar@gmail.com>
2014-01-13 17:56:52 +0800

    Add 'for' in template
    Signed-off-by: Wang <wangwangwar@gmail.com>

--- app/views.py
+++ app/views.py
@@ -6,5 +6,16 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'nickname': 'Shulin'}
+    posts = [   # fake array of posts
+        {
+            'author': {'nickname': 'Li'},
+            'body': "彪悍的人生何须BB。"
+        },
+        {
+            'author': {'nickname': 'Zhang'},
+            'body': "大家好你是猪嗦。"
+        }]
     return render_template('index.html',
-                           user=user)
+                           title='Home',
+                           user=user,
+                           posts=posts)
