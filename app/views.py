c70e82cd3892114b81d17dd49f621f2ef4590ea5
Wang <wangwangwar@gmail.com>
2014-01-14 10:33:00 +0800

    Receive form data
    We receive form data in func login(), and use 2 new flask func,
    * flash
    * redirect
    
    Signed-off-by: Wang <wangwangwar@gmail.com>

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 
@@ -25,6 +25,11 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' +
+              form.openid.data + '", remember_me=' +
+              str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
                            form=form)
