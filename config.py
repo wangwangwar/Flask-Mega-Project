29828b689ef138fa5a221897cc9d52a126b88bcc
Wang <wangwangwar@gmail.com>
2014-01-14 10:56:38 +0800

    Deal with OpenIDs
    Signed-off-by: Wang <wangwangwar@gmail.com>

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 CSRF_ENABLE = True
 SECRET_KEY = 'who is your daddy?'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
