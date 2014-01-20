01c75497e826eeea3ae7d24ab5f3deffc69f3503
Wang <wangwangwar@gmail.com>
2014-01-20 10:54:35 +0800

    Fix manage.py bug
    In the past, we call `db.create_all()`, which is totally wrong!
    We HAVE TO initial migration for the first time:
    `python manage.py db init`
    then migrate and
     upgrade explicitly each time we update the model:
    `python manage.py db migrate`
    `python manage.py db upgrade`
    
    Signed-off-by: Wang <wangwangwar@gmail.com>

--- manage.py
+++ manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 from app import app, db
-from app.models import User, Post
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-
-db.create_all()
 migrate = Migrate(app, db)
 
 manager = Manager(app)
