2081e183d6d9468654c3c8145db2f602b837b59c
Wang <wangwangwar@gmail.com>
2014-01-20 15:15:23 +0800

    Add 'follow' and 'unfollow' functions with unit test

--- app/models.py
+++ app/models.py
@@ -62,6 +62,20 @@ class User(db.Model):
             version += 1
         return new_nickname
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return (self.followed.filter(followers.c.followed_id == user.id).
+                count() > 0)
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
--- test.py
+++ test.py
@@ -11,7 +11,8 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(basedir, 'test.db')
         self.app = app.test_client()
         db.create_all()
 
@@ -22,7 +23,8 @@ class TestCase(unittest.TestCase):
     def test_avatar(self):
         u = User(nickname='john', email='john@example.com')
         avatar = u.avatar(128)
-        expected = 'http://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6'
+        expected = \
+            'http://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6'
         assert avatar[0:len(expected)] == expected
 
     def test_make_unique_nickname(self):
@@ -37,6 +39,30 @@ class TestCase(unittest.TestCase):
         nickname2 = User.make_unique_nickname('john')
         assert nickname2 != 'john'
         assert nickname2 != nickname
+
+    def test_follow(self):
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.unfollow(u2) is None
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+        assert u1.follow(u2) is None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().nickname == 'john'
+        u = u1.unfollow(u2)
+        assert u is not None
+        db.session.add(u)
+        db.session.commit()
+        assert u1.is_following(u2) is False
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
 
 if __name__ == '__main__':
     unittest.main()
