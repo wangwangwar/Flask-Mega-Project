4eb7d16effbccf478aa9c8bc8ca1983fd32c6f76
Wang <wangwangwar@gmail.com>
2014-01-20 11:08:16 +0800

    The Flask Mega-Tutorial, Part VIII: Followers, Contacts And Friends
    * Add 'followers' model.
    * Do 'migrate' and 'upgrade'.
    
    Signed-off-by: Wang <wangwangwar@gmail.com>

--- app/models.py
+++ app/models.py
@@ -6,6 +6,15 @@ ROLE_USER = 0
 ROLE_ADMIN = 1
 
 
+followers = db.Table('followers',
+                     db.Column('follower_id',
+                               db.Integer,
+                               db.ForeignKey('user.id')),
+                     db.Column('followed_id',
+                               db.Integer,
+                               db.ForeignKey('user.id')))
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -14,6 +23,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     def is_authenticated(self):
         return True
