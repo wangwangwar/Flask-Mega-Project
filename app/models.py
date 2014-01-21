a55d4fc88258067780e53b6b3673e86a882c2966
Wang <wangwangwar@gmail.com>
2014-01-21 15:47:07 +0800

    Add followed posts query function with unit test

--- app/models.py
+++ app/models.py
@@ -76,6 +76,12 @@ class User(db.Model):
         return (self.followed.filter(followers.c.followed_id == user.id).
                 count() > 0)
 
+    def followed_posts(self):
+        return (Post.query.
+                join(followers, (followers.c.followed_id == Post.user_id))
+                .filter(followers.c.follower_id == self.id).
+                order_by(Post.timestamp.desc()))
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
