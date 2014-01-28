d8016edc7582b4f74ec6a492d838ecd3f34bd0ce
Wang <wangwangwar@gmail.com>
2014-01-28 15:10:12 +0800

    Make Sending Email Asynchronous
    We use thread for sending email background.

--- app/emails.py
+++ app/emails.py
@@ -1,14 +1,21 @@
 from flask.ext.mail import Message
 from flask import render_template
-from app import mail
+from app import mail, app
 from config import ADMINS
+from threading import Thread
+
+
+def send_async_email(msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_mail(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[msg])
+    thr.start()
 
 
 def follower_notification(followed, follower):
